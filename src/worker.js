c13b6d1fc3c2ee8516442463169918c2b927d698
lixue <lixue@zcool.com.cn>
2022-05-23 14:02:50 +0800

    fix: target es5

--- src/worker.js
+++ src/worker.js
@@ -1,46 +1,46 @@
-const workerContent = `
-importScripts('https://cdnout.com/spark-md5/')
-onmessage = function (e) {
-  function md5File(file) {
-    const proto = Object.getPrototypeOf(file)
-    const blobSlice = proto.slice || proto.mozSlice || proto.webkitSlice,
-      chunkSize = 2097152, // Read in chunks of 2MB
-      chunks = Math.ceil(file.size / chunkSize),
-      spark = new SparkMD5.ArrayBuffer(),
-      fileReader = new FileReader()
-    let currentChunk = 0
+export default function createWorker() {
+  /* separator */
+  importScripts('https://cdnout.com/spark-md5/')
+  onmessage = function (e) {
+    function md5File(file) {
+      const proto = Object.getPrototypeOf(file)
+      const blobSlice = proto.slice || proto.mozSlice || proto.webkitSlice,
+        chunkSize = 2097152, // Read in chunks of 2MB
+        chunks = Math.ceil(file.size / chunkSize),
+        spark = new SparkMD5.ArrayBuffer(),
+        fileReader = new FileReader()
+      let currentChunk = 0
 
-    fileReader.onload = (e) => {
-      spark.append(e.target.result) // Append array buffer
-      currentChunk++
-      if (currentChunk < chunks) {
-        loadNext()
-      } else {
-        postMessage(spark.end())
-        close()
+      fileReader.onload = (event) => {
+        spark.append(event.target.result) // Append array buffer
+        currentChunk += 1
+        if (currentChunk < chunks) {
+          loadNext()
+        } else {
+          postMessage(spark.end())
+          close()
+        }
       }
+
+      fileReader.onerror = function () {
+        postMessage('')
+        close()
+        throw new Error(fileReader.error)
+      }
+
+      function loadNext() {
+        var start = currentChunk * chunkSize,
+          end = start + chunkSize >= file.file_size ? file.file_size : start + chunkSize
+
+        fileReader.readAsArrayBuffer(blobSlice.call(file, start, end))
+      }
+
+      loadNext()
     }
 
-    fileReader.onerror = function () {
-      postMessage('')
-      close()
-      throw fileReader.error
+    if (e.data && e.data.file) {
+      md5File(e.data.file)
     }
-
-    function loadNext() {
-      const start = currentChunk * chunkSize,
-        end = start + chunkSize >= file.file_size ? file.file_size : start + chunkSize
-
-      fileReader.readAsArrayBuffer(blobSlice.call(file, start, end))
-    }
-
-    loadNext()
   }
-
-  if (e.data && e.data.file) {
-    md5File(e.data.file)
-  }
+  /* separator */
 }
-`
-
-export default workerContent
