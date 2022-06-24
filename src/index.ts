bd3ed27d80b5d268854334601b2c51e53801fc61
pangxiaolong <>
2022-06-24 11:40:44 +0800

    fix: fix video delete

--- src/index.ts
+++ src/index.ts
@@ -123,13 +123,13 @@ export default class HFUploader {
   // 清空
   clear = () => {
     this.delIds = this.fileList.map((v) => v.uid)
-    this.fileList = []
-    this.queue.clear()
     for (const key in this.map) {
       if (this.map[key]) {
         this.map[key].deleteUpload()
       }
     }
+    this.queue.clear()
+    this.fileList = []
   }
 
   // 重新上传
